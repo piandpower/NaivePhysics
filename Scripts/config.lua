2459da228086c9c4d9dfb8d4dde94bf78f9c72bf
marioyc <ycmario@gmail.com>
2016-06-10 12:19:40 +0200

    Move config

---
+++ Scripts/config.lua
@@ -0,0 +1,32 @@
+config = {
+	--seed = 0,
+	dataPath = '/home/mario/Documents/Unreal Projects/NaivePhysics/data/', -- don't override anything important
+	screenCaptureInterval = 0.125,
+	sceneTime = 15.0,
+	stride = 5,
+	iterations = 10
+	--loadTime = 2.0, -- 1s
+	--resolution = 'nil', -- this gets interpreted by the blueprints as NULL, but you can still override
+}
+
+function getDataPath()
+	return config['dataPath']
+end
+
+function getScreenCaptureInterval()
+	return config['screenCaptureInterval']
+end
+
+function getSceneTime()
+	return config['sceneTime']
+end
+
+function getStride()
+	return config['stride']
+end
+
+function doIteration()
+	local iteration = config['iterations']
+	config['iterations'] = config['iterations'] - 1
+	return iteration
+end
--- Scripts/naive_physics.lua
+++ Scripts/naive_physics.lua
@@ -1,26 +1,8 @@
 require 'uetorch'
 image = require 'image'
+require 'config'
 
 SetTickDeltaBounds(1/16,1/16)
-
-config = {
-	--seed = 0,
-	dataRoot = '/home/mario/Documents/Unreal Projects/NaivePhysics/data', -- don't override anything important
-	screenCaptureTime = 0.125,
-	sceneTime = 15.0,
-	stride = 5
-	--loadTime = 2.0, -- 1s
-	--resolution = 'nil', -- this gets interpreted by the blueprints as NULL, but you can still override
-}
-
-for k,v in pairs(config) do
-	if os.getenv(k) then
-		local v = os.getenv(k)
-		config[k] = tonumber(v) or v
-	end
-end
-
-print(config)
 
 local ground_materials = {"M_Basic_Floor", "M_Ground_Grass", "M_Ground_Moss", "M_Wood_Floor_Walnut_Polished", "M_Wood_Floor_Walnut_Worn"}
 
@@ -85,8 +67,8 @@ local it = 0
 function saveScreen(dt)
 	t3 = t3 + dt
 
-	if t3 - last_save_t >= config['screenCaptureTime'] then
-		local file1 = config['dataRoot'] .. '/' .. it .. '_screen.jpg'
+	if t3 - last_save_t >= getScreenCaptureInterval() then
+		local file1 = getDataPath() .. it .. '_screen.jpg'
 		print(file1,t3,dt)
 	    local i1 = Screen()
 
@@ -95,15 +77,15 @@ function saveScreen(dt)
 	    end
 
 		local actors = {obj, wall}
-	    local file2 = config['dataRoot'] .. '/' .. it .. '_objseg.jpg'
-	    local i2 = ObjectSegmentation(actors, config['stride'])
+	    local file2 = getDataPath() .. it .. '_objseg.jpg'
+	    local i2 = ObjectSegmentation(actors, getStride())
 
 	    if i2 then
 			image.save(file2,i2)
 	    end
 
-	    local file3 = config['dataRoot'] .. '/' .. it .. '_mask.jpg'
-	    local i3 = ObjectMasks(actors, config['stride'])
+	    local file3 = getDataPath() .. it .. '_mask.jpg'
+	    local i3 = ObjectMasks(actors, getStride())
 
 	    if i3 then
 			image.save(file3,i3[1])
@@ -113,7 +95,7 @@ function saveScreen(dt)
 		last_save_t = t3
 	end
 
-	if t3 >= config['sceneTime'] then
+	if t3 >= getSceneTime() then
 		RemoveHook(saveScreen)
 	end
 end
