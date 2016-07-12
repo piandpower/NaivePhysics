75b46f23cada5b97d575d8777b1c9a308dd9c8ea
marioyc <ycmario@gmail.com>
2016-07-12 18:11:01 +0200

    save tuples to videos

--- imageseq_to_video.py
+++ imageseq_to_video.py
@@ -1,24 +1,34 @@
 import cv2
 
 path = "data/"
-suffix = "_screen"
-width, height = 1171, 584
+prefix = "screen_"
+width, height = 1128, 555
 fps = 24
 
 ndata = 12
 
 for i in range(1,1 + ndata):
-	video = cv2.VideoWriter(path + '/video' + str(i) + '.avi', cv2.cv.CV_FOURCC('M','J','P','G'), fps, (width,height))
-	pos = 1
+	tuple_pos = 0
 
 	while True:
-		name = path + str(i) + "/" + str(pos) + suffix + '.jpg'
+		name = path + str(i) + "/" + prefix + "1_" + str(tuple_pos) + ".jpg"
 		img = cv2.imread(name)
 
 		if img is None:
 			break
 
-		video.write(img)
-		pos += 1
+		video = cv2.VideoWriter(path + '/video_' + str(i) + "_" + str(tuple_pos) + '.avi', cv2.cv.CV_FOURCC('M','J','P','G'), fps, (width,height))
+		pos = 1
 
-	video.release()
+		while True:
+			name = path + str(i) + "/" + prefix + str(pos) + "_" + str(tuple_pos) + '.jpg'
+			img = cv2.imread(name)
+
+			if img is None:
+				break
+
+			video.write(img)
+			pos += 1
+
+		video.release()
+		tuple_pos += 1
