d0a09d4a6bdc1389e9101d4df4f32b2cf9af0e37
Mathieu Bernard <mathieu_bernard@laposte.net>
2016-11-02 13:43:01 +0100

    read iterations from config.json

--- Scripts/config.lua
+++ Scripts/config.lua
@@ -1,7 +1,44 @@
+local lfs = require 'lfs'
+local json = require 'cjson'
+local uetorch = require 'uetorch'
+
 local config = {}
 
+
+-- Exit the program
+local function Exit()
+   uetorch.ExecuteConsoleCommand('Exit')
+end
+
+
+-- Pad a number with beginning zeros, return it as a string
+local function PadZeros(int, n)
+   s = tostring(int)
+   for _ = 1, n-#s do
+      s = '0' .. s
+   end
+   return s
+end
+
+
+-- Load a JSon file as a table
+local function ReadJson(file)
+   local f = assert(io.open(file, "rb"))
+   local content = f:read("*all")
+   f:close()
+   return json.decode(content)
+end
+
+
+-- Write a table as a JSon file
+local function WriteJson(t, file)
+   local f = assert(io.open(file, "wb"))
+   f:write(json.encode(t))
+   f:close()
+end
+
+
 conf = {
-   --   dataPath = '/home/mario/Documents/Unreal Projects/NaivePhysics/data/',
    dataPath = assert(os.getenv('NAIVEPHYSICS_DATA')),
    loadParams = false,
    stitch = false,
@@ -29,89 +66,95 @@ conf = {
       block1c = 4,
       block5a = 2
    },
-   blocks = {
-      {
-         iterations = 1,
-         block = 'blockC1_static'
-      },
-      {
-         iterations = 1,
-         block = 'blockC1_dynamic_1'
-      },
-      {
-         iterations = 1,
-         block = 'blockC1_dynamic_2'
-      },
-      {
-         iterations = 0,
-         block = 'block1c'
-      },
-      {
-         iterations = 0,
-         block = 'block5a'
-      }
-   }
+   blocks = ReadJson(assert(os.getenv('NAIVEPHYSICS_JSON')))
 }
 
+
+
 function SetIterationsCounter()
-   local iterations = 0
-   for k, v in ipairs(conf.blocks) do
-      local block = v.block
-      iterations = iterations + v.iterations * (conf.tupleSize[block] + conf.visibilityCheckSize[block])
+   -- count the number of iterations both for train and test. Each
+   -- train is always a single iteration, the number of test
+   -- iterations depends on the block type
+   local train_runs, test_runs, test_iterations = 0, 0, 0
+   for block, iters in pairs(config.GetBlocks()) do
+      train_runs = train_runs + assert(iters.train)
+      test_runs = test_runs + assert(iters.test)
    end
-   print("iterations = " .. iterations)
-   local file = conf.dataPath .. 'iterations.t7'
-   torch.save(file, iterations)
-   return iterations
+
+   if test_runs + train_runs == 0 then
+      print('no iterations specified, exiting')
+      Exit()
+   end
+   print("generation of " .. test_runs .. " test and " .. train_runs .. " train samples")
+
+   -- put the detail of each iteration into a table
+   local n, iterationsTable = 1, {}
+   for block, iters in pairs(config.GetBlocks()) do
+      -- setup train iterations for the current block
+      for id = 1, iters.train do
+         iterationsTable[n] = {iterationBlock=block, iterationType=-1, iterationId=id}
+         n = n + 1
+      end
+
+      -- setup test iterations for the current block
+      local ntypes = config.GetTupleSize(block) + config.GetVisibilityCheckSize(block)
+      for id = 1, iters.test do
+         for t = ntypes, 1, -1 do
+            iterationsTable[n] = {iterationBlock=block, iterationType=t, iterationId=id}
+            n = n + 1
+         end
+      end
+   end
+
+   WriteJson(iterationsTable, conf.dataPath .. 'iterations_table.json')
+   torch.save(conf.dataPath .. 'iterations.t7', "1")
 end
+
 
 function config.GetDataPath()
    return conf.dataPath
 end
 
+
 function config.GetLoadParams()
    return conf.loadParams
 end
+
 
 function config.GetStitch()
    return conf.stitch
 end
 
+
 function config.GetBlockCaptureInterval(block)
    return conf.captureInterval[block]
 end
+
 
 function config.GetBlockTicks(block)
    return conf.sceneTicks[block]
 end
 
-function config.GetIterationInfo(iteration)
-   iteration = tonumber(iteration)
-   local iterationId = 0
-   for k, v in ipairs(conf.blocks) do
-      local block = v.block
-      local block_len = conf.tupleSize[block] + conf.visibilityCheckSize[block]
-      local cur = v.iterations * block_len
 
-      if iteration <= cur then
-         iterationId = iterationId + math.ceil(iteration / block_len)
-         local iterationType = iteration % block_len
-         if iterationType == 0 then
-            iterationType = block_len
-         end
-         return iterationId, iterationType, block
-      end
-
-      iteration = iteration - cur
-      iterationId = iterationId + v.iterations
-   end
-   print("ERROR: Invalid Iteration")
-   return nil
+function config.GetBlocks()
+   return conf.blocks
 end
+
+
+function config.GetTupleSize(block)
+   return conf.tupleSize[block]
+end
+
+
+function config.GetVisibilityCheckSize(block)
+   return conf.visibilityCheckSize[block]
+end
+
 
 function config.GetBlockSize(block)
    return conf.visibilityCheckSize[block] + conf.tupleSize[block]
 end
+
 
 function config.IsVisibilityCheck(block, iterationType)
    if conf.visibilityCheckSize[block] == 0 then
@@ -123,5 +166,44 @@ function config.IsVisibilityCheck(block, iterationType)
       return false
    end
 end
+
+
+local iterationsTable, maxId = nil, nil
+
+function config.GetIterationInfo(iteration)
+   if not iterationsTable then
+      iterationsTable = ReadJson(conf.dataPath .. 'iterations_table.json')
+      maxId = 0
+      for k, v in pairs(conf.blocks) do
+         maxId = math.max(maxId, v.test, v.train)
+      end
+   end
+
+   local i = iterationsTable[tonumber(iteration)]
+   if not i then
+      print('no more iterations, exiting')
+      Exit()
+   else
+      subpath = 'train/'
+      if i.iterationType ~= -1 then
+         subpath = 'test/'
+      end
+
+      -- TODO do not create directories here but in SetCurrentIteration
+      path = conf.dataPath .. subpath
+      lfs.mkdir(path)
+
+      path = path .. PadZeros(i.iterationId, #tostring(maxId)) .. '/'
+      lfs.mkdir(path)
+
+      if i.iterationType ~= -1 then
+         path = path .. i.iterationType .. '/'
+         lfs.mkdir(path)
+      end
+
+      return i.iterationId, i.iterationType, i.iterationBlock, path
+   end
+end
+
 
 return config
--- Scripts/utils.lua
+++ Scripts/utils.lua
@@ -2,7 +2,12 @@ local uetorch = require 'uetorch'
 local config = require 'config'
 local utils = {}
 
-utils.ground_materials = {"M_Basic_Floor", "M_Ground_Grass", "M_Ground_Moss", "M_Wood_Floor_Walnut_Polished", "M_Wood_Floor_Walnut_Worn"}
+
+utils.ground_materials = {
+   "M_Basic_Floor", "M_Ground_Grass",
+   "M_Ground_Moss", "M_Wood_Floor_Walnut_Polished",
+   "M_Wood_Floor_Walnut_Worn"}
+
 
 function utils.SetActorMaterial(actor, id)
    local materialId = "Material'/Game/StarterContent/Materials/" .. id .. "." .. id .. "'"
@@ -10,28 +15,36 @@ function utils.SetActorMaterial(actor, id)
    uetorch.SetMaterial(actor, material)
 end
 
+
 function utils.GetCurrentIteration()
    local iteration = torch.load(conf.dataPath .. 'iterations.t7')
    return iteration
 end
 
+
 local function GetFirstIterationInBlock(iteration)
    local iterationId, iterationType, block = config.GetIterationInfo(iteration)
-   return iteration + config.GetBlockSize(block) - iterationType
+   return iteration - config.GetBlockSize(block) + iterationType
 end
+
 
 function utils.UpdateIterationsCounter(check)
    local iteration = utils.GetCurrentIteration()
-   local iterationId, iterationType, iterationBlock = config.GetIterationInfo(iteration)
+   local iterationId, iterationType, iterationBlock, iterationPath
+      = config.GetIterationInfo(iteration)
 
    if check then
-      iteration = iteration - 1
+      iteration = iteration + 1
    else
+      print('check failed, rerun the iteration with new parameters')
       iteration = GetFirstIterationInBlock(iteration)
    end
 
+   -- TODO ensure the iteration exists in iterationsTable (for now
+   -- done in GetIterationinfo)
    torch.save(conf.dataPath .. 'iterations.t7', iteration)
 end
+
 
 local TickHooks = {}
 local EndTickHooks = {}
@@ -42,9 +55,11 @@ function utils.AddTickHook(f)
    table.insert(TickHooks, f)
 end
 
+
 function utils.AddEndTickHook(f)
    table.insert(EndTickHooks, f)
 end
+
 
 -- remove the function f from the set of tick hooks
 function utils.RemoveTickHook(f)
@@ -55,12 +70,14 @@ function utils.RemoveTickHook(f)
    end
 end
 
+
 local TicksRemaining
 
 function utils.SetTicksRemaining(ticks)
    print("TicksRemaining : " .. ticks)
    TicksRemaining = ticks
 end
+
 
 local tickCount = 1
 local foundError = false
