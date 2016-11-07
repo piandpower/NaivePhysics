599a0ab6c38abfdf2486c60406d8b940dfe2d766
Mathieu Bernard <mathieu_bernard@laposte.net>
2016-11-07 14:46:24 +0100

    add ground materials, FindObject -> LoadObject

--- Scripts/material.lua
+++ Scripts/material.lua
@@ -3,14 +3,26 @@ local material = {}
 
 
 material.ground_materials = {
-   "M_Basic_Floor", "M_Ground_Grass",
-   "M_Ground_Moss", "M_Wood_Floor_Walnut_Polished",
-   "M_Wood_Floor_Walnut_Worn"}
+   "M_Basic_Floor",
+   "M_Brick_Clay_Beveled",
+   "M_Brick_Clay_New",
+   "M_Brick_Clay_Old",
+   "M_Brick_Cut_Stone",
+   "M_Ground_Grass",
+   "M_Ground_Gravel",
+   "M_Ground_Moss",
+   "M_Tech_Panel",
+   "M_Wood_Wallnut",
+   "M_Wood_Oak",
+   "M_Wood_Pine",
+   "M_Wood_Floor_Walnut_Polished",
+   "M_Wood_Floor_Walnut_Worn"
+}
 
 
 function material.SetActorMaterial(actor, id)
    local materialId = "Material'/Game/StarterContent/Materials/" .. id .. "." .. id .. "'"
-   local material = UE.FindObject(Material.Class(), nil, materialId)
+   local material = UE.LoadObject(Material.Class(), nil, materialId)
    uetorch.SetMaterial(actor, material)
 end
 
