492819adbaeed79bdf52d18f9d530ff3d23da3e9
Mathieu Bernard <mathieu_bernard@laposte.net>
2016-11-22 14:26:05 +0100

    update image2video.sh
    now recursive and generate playlist

--- images2video.sh
+++ images2video.sh
@@ -16,7 +16,10 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #
-# Convert the png files in a directory into a mp4 video using avconv
+# Convert the png files in a directory (and recursivly in
+# subdirectories) into a mp4 video using avconv (one video per
+# directory). This also create a m3u playlist of the generated videos
+# in $data_dir
 #
 
 # make sure avconv is installed
@@ -25,41 +28,47 @@
     && echo "Please run 'sudo apt-get install libav-tools'" \
     && exit 1
 
-# remove any trailing slash
-data_dir=${1%/}
+# remove any trailing slash and make it absolute
+data_dir=$(readlink -f ${1%/})
 
 # display a usage message if bad params
-[ -z "$data_dir" ] && echo "Usage: $0 directory" && exit 0
+[ -z "$data_dir" ] && echo "Usage: $0 <directory>" && exit 0
 
 # display error message if input is not a directory
 [ ! -d "$data_dir" ] && echo "Error: $data_dir is not a directory"  && exit 1
 
-# display error message if avconv is not installed
-[ -z $(which avconv) ] && echo "Error: avconv is not installed on your system"  && exit 1
+# list all subdirectories containing at least one png file
+png_dirs=$(find $data_dir -type f -name "*.png" -exec dirname {} \; | uniq)
 
-# list all png images in the directory
-png=$(ls $data_dir/*.png 2> /dev/null)
+# display error message if no png found
+[ -z "$png_dirs" ] && echo "Error: no png file in $data_dir" && exit 1
 
-# display error message if no png in the directory
-[ -z "$png" ] && echo "Error: no png file in $data_dir" && exit 1
+rm -f $data_dir/playlist.m3u
+for dir in $png_dirs;
+do
+    # list all png images in the directory
+    png=$(ls $dir/*.png 2> /dev/null)
 
-# get the first png file in the list
-first=$(echo $png | cut -f1 -d' ')
+    # get the first png file in the list
+    first=$(echo $png | cut -f1 -d' ')
 
-# find the length of the images index (just consider the first png, we
-# assume they all have same index length)
-index=$(echo $first | sed -r 's|^.+_([0-9]+)\.png$|\1|g')
-n=${#index}
+    # find the length of the images index (just consider the first png, we
+    # assume they all have same index length)
+    index=$(echo $first | sed -r 's|^.+_([0-9]+)\.png$|\1|g')
+    n=${#index}
 
-# png files basename, with extension and index removed
-base=$(basename $first | sed -r 's|^(.+_)[0-9]+\.png$|\1|g')
+    # png files basename, with extension and index removed
+    base=$(basename $first | sed -r 's|^(.+_)[0-9]+\.png$|\1|g')
 
-# the global pattern matching png files for avconv
-pattern=$(echo $data_dir/$base%0${n}d.png)
+    # the global pattern matching png files for avconv
+    pattern=$(echo $dir/$base%0${n}d.png)
 
-# convert the png images into a video.avi
-avconv -y -framerate 24 -i $pattern -c:v libx264 -r 30 -pix_fmt yuv420p $data_dir/video.avi \
-       || (echo "Error: failed to write video from $pattern"; exit 1)
+    # convert the png images into a video.avi
+    avconv -y -framerate 24 -i $pattern -c:v libx264 -r 30 -pix_fmt yuv420p $dir/video.avi \
+        || (echo "Error: failed to write video from $pattern"; exit 1)
 
-echo "Wrote $data_dir/video.avi"
+    echo "Wrote $dir/video.avi"
+    echo "$dir/video.avi" >> $data_dir/playlist.m3u
+done
+
 exit 0
