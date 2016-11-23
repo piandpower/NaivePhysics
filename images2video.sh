9b1ecd4cb22f51fd0148257dbef6e1d53cae99e6
Mathieu Bernard <mathieu_bernard@laposte.net>
2016-11-23 16:28:12 +0100

    images2video.sh now supports gif output

--- images2video.sh
+++ images2video.sh
@@ -17,22 +17,40 @@
 
 #
 # Convert the jpeg files in a directory (and recursivly in
-# subdirectories) into a mp4 video using avconv (one video per
-# directory). This also create a m3u playlist of the generated videos
-# in $data_dir
+# subdirectories) into a video (one video per directory and a m3u
+# playlist) or into a gif.
 #
 
-# make sure avconv is installed
-[ -z $(which avconv 2> /dev/null) ] \
-    && echo "Error: avconv not installed on your system." \
-    && echo "Please run 'sudo apt-get install libav-tools'" \
-    && exit 1
+# display a usage message if bad params
+[ $# -lt 1 ] \
+    && echo "Usage: $0 <directory> [--gif]" \
+    && echo "Find subdirectories of <directory> which contain jpeg files and \
+convert them to video (or gif if the --gif option is specified as second \
+argument). Create one video in each subdirectory." \
+    && exit 0
+
 
 # remove any trailing slash and make it absolute
 data_dir=$(readlink -f ${1%/})
 
-# display a usage message if bad params
-[ -z "$data_dir" ] && echo "Usage: $0 <directory>" && exit 0
+# output format is either "video" or "gif" (if --gif specified to $2)
+if [[ "$2" == *gif* ]]; then
+    format="gif"
+
+    # make sure convert is installed (for gif conversion)
+    [ -z $(which convert 2> /dev/null) ] \
+        && echo "Error: convert not installed on your system." \
+        && echo "Please run 'sudo apt-get install imagemagick'" \
+        && exit 1
+else
+    format="video"
+
+    # make sure avconv is installed (for avi conversion)
+    [ -z $(which avconv 2> /dev/null) ] \
+        && echo "Error: avconv not installed on your system." \
+        && echo "Please run 'sudo apt-get install libav-tools'" \
+        && exit 1
+fi
 
 # display error message if input is not a directory
 [ ! -d "$data_dir" ] && echo "Error: $data_dir is not a directory"  && exit 1
@@ -43,32 +61,46 @@ jpeg_dirs=$(find $data_dir -type f -name "*.jpeg" -exec dirname {} \; | uniq)
 # display error message if no jpeg found
 [ -z "$jpeg_dirs" ] && echo "Error: no jpeg file in $data_dir" && exit 1
 
+# overwrite any existing playlist
 rm -f $data_dir/playlist.m3u
+
+# process each jpeg directory
 for dir in $jpeg_dirs;
 do
-    # list all jpeg images in the directory
-    jpeg=$(ls $dir/*.jpeg 2> /dev/null)
+    case $format in
+        "video"*)
+            # list all jpeg images in the directory
+            jpeg=$(ls $dir/*.jpeg 2> /dev/null)
 
-    # get the first jpeg file in the list
-    first=$(echo $jpeg | cut -f1 -d' ')
+            # get the first jpeg file in the list
+            first=$(echo $jpeg | cut -f1 -d' ')
 
-    # find the length of the images index (just consider the first jpeg, we
-    # assume they all have same index length)
-    index=$(echo $first | sed -r 's|^.+_([0-9]+)\.jpeg$|\1|g')
-    n=${#index}
+            # find the length of the images index (just consider the first jpeg, we
+            # assume they all have same index length)
+            index=$(echo $first | sed -r 's|^.+_([0-9]+)\.jpeg$|\1|g')
+            n=${#index}
 
-    # jpeg files basename, with extension and index removed
-    base=$(basename $first | sed -r 's|^(.+_)[0-9]+\.jpeg$|\1|g')
+            # jpeg files basename, with extension and index removed
+            base=$(basename $first | sed -r 's|^(.+_)[0-9]+\.jpeg$|\1|g')
 
-    # the global pattern matching jpeg files for avconv
-    pattern=$(echo $dir/$base%0${n}d.jpeg)
+            # the global pattern matching jpeg files for avconv
+            pattern=$(echo $dir/$base%0${n}d.jpeg)
 
-    # convert the jpeg images into a video.avi
-    avconv -y -framerate 24 -i $pattern -c:v libx264 -r 30 -pix_fmt yuv420p $dir/video.avi \
-        || (echo "Error: failed to write video from $pattern"; exit 1)
+            # convert the jpeg images into a video.avi
+            avconv -y -framerate 24 -i $pattern -c:v libx264 -r 30 -pix_fmt yuv420p $dir/video.avi \
+                || (echo "Error: failed to write video from $pattern"; exit 1)
+            echo "Wrote $dir/video.avi"
+            echo "$dir/video.avi" >> $data_dir/playlist.m3u
+            ;;
+        "gif"*)
+            # convert the jpeg sequence to a video.gif
+            convert -delay 10 -loop 0 $dir/*.jpeg $dir/video.gif
+            echo "Wrote $dir/video.gif"
+            echo "$dir/video.gif" >> $data_dir/playlist.m3u
+            ;;
+    esac
 
-    echo "Wrote $dir/video.avi"
-    echo "$dir/video.avi" >> $data_dir/playlist.m3u
+
 done
 
 exit 0
