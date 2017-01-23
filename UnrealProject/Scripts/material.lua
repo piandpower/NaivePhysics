e5bb21fce0933c17a28293d3d1a8f8ac29c2448a
Mathieu Bernard <mathieu_bernard@laposte.net>
2017-01-23 17:07:51 +0100

    new materials added

--- UnrealProject/Scripts/material.lua
+++ UnrealProject/Scripts/material.lua
@@ -19,15 +19,22 @@ material.ground_materials = {
    "M_Wood_Oak",
    "M_Wood_Pine",
    "M_Wood_Walnut",
+   "M_ConcreteTile",
+   "M_Concrete_Tiles",
+   "M_Floor_01",
+   "M_FloorTile_02",
+   "M_GroundSand_01",
+   "M_SoilMud01",
+   "M_SoilSand_01"
 }
 
 
 material.sphere_materials = {
    "BlackMaterial",
    "GreenMaterial",
-   -- "M_ColorGrid_LowSpec",
-   -- "M_Metal_Brushed_Nickel",
-   -- "M_Metal_Burnished_Steel",
+   "M_ColorGrid_LowSpec",
+   "M_Metal_Brushed_Nickel",
+   "M_Metal_Burnished_Steel",
    -- "M_Metal_Chrome",
    "M_Metal_Copper",
    "M_Metal_Gold",
@@ -35,6 +42,8 @@ material.sphere_materials = {
    "M_Metal_Steel",
    "M_Tech_Hex_Tile",
    "M_Tech_Panel",
+   "Base_Colour",
+   "M_MetalFloor_01",
 }
 
 
@@ -49,11 +58,15 @@ material.wall_materials = {
    "M_CobbleStone_Pebble",
    "M_CobbleStone_Rough",
    "M_CobbleStone_Smooth",
+   "M_Bricks_1",
+   "M_Bricks_2",
+   "M_Bricks_3",
+   "M_Bricks_4"
 }
 
 
 function material.SetActorMaterial(actor, id)
-   local materialId = "Material'/Game/StarterContent/Materials/" .. id .. "." .. id .. "'"
+   local materialId = "Material'/Game/Materials/" .. id .. "." .. id .. "'"
    local material = UE.LoadObject(Material.Class(), nil, materialId)
    uetorch.SetMaterial(actor, material)
 end
